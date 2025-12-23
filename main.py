e89336e5ef8b9c2bf025ca388f5056bba63a2f77
hajko <julian_ong@yahoo.com>
2025-12-22 17:16:45 -0800

    B: passing submission

--- main.py
+++ main.py
@@ -26,39 +26,62 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions], system_instruction=system_prompt
-        ),
-    )
+    MAX_ITERATIONS = 20
+    try:
+        for _ in range(MAX_ITERATIONS):
+            response = client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents=messages,
+                config=types.GenerateContentConfig(
+                    tools=[available_functions], system_instruction=system_prompt
+                ),
+            )
 
-    if response.usage_metadata != None:
-        if args.verbose:
-            print(f"User prompt: {args.user_prompt}")
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        raise RuntimeError("no usage metadata found")
-
-    # if response.function_calls != None:
-    #     for function_call in response.function_calls:
-    #         print(f"Calling function: {function_call.name}({function_call.args})")
-    # else:
-    #     print(response.text)
-
-    response_list = []
-    if response.function_calls != None:
-        for function_call in response.function_calls:
-            response = call_function(function_call)
-            if not response.parts[0].function_response.response:
-                raise Exception("error: no response")
-            else:
-                response_list.append(response.parts[0])
-                print(f"Calling function: {function_call.name}({function_call.args})")
+            if response.usage_metadata != None:
                 if args.verbose:
-                    print(f"-> {response.parts[0].function_response.response}")
+                    print(f"User prompt: {args.user_prompt}")
+                    print(
+                        f"Prompt tokens: {response.usage_metadata.prompt_token_count}"
+                    )
+                    print(
+                        f"Response tokens: {response.usage_metadata.candidates_token_count}"
+                    )
+            else:
+                raise RuntimeError("no usage metadata found")
+
+            if not response.candidates:
+                print("no candidates returned")
+                break
+            else:
+                for candidate in response.candidates:
+                    messages.append(candidate.content)
+
+            if response.function_calls is None:
+                if response.text:
+                    print(response.text)
+                break
+            else:
+                function_call_responses = []
+                for function_call in response.function_calls:
+                    function_call_response = call_function(function_call)
+                    if not function_call_response.parts[0].function_response.response:
+                        break
+                    else:
+                        function_call_responses.append(function_call_response.parts[0])
+                        print(
+                            f"Calling function: {function_call.name}({function_call.args})"
+                        )
+                        if args.verbose:
+                            print(f"-> {response.parts[0].function_response.response}")
+
+                function_call_responses_msg = types.Content(
+                    role="user",
+                    parts=function_call_responses,
+                )
+
+                messages.append(function_call_responses_msg)
+    except Exception as e:
+        print(f"error in generate_content: {e}")
 
 
 if __name__ == "__main__":
